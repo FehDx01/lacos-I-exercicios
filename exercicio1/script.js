984764451bafedbafc72a0aa74457d1fa2d9f485
FehDx01 <lessa.fernando01@gmail.com>
2023-12-10 10:01:04 -0300

    exercício laços I

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+
+/* let comerCoxinha = prompt("Você deseja comer mais coxinhas?").toLowerCase
+
+let conta = 0
+
+while (comerCoxinha !== "n") {
+        conta += 2.50
+        
+        comerCoxinha++
+
+        comerCoxinha = prompt("Você deseja comer mais coxinhas?")
+        
+        console.log(`Sua conta está no valor de R$ ${conta}`)
+}
+
+console.log(`Sua conta ficou no valor de R$ ${conta}`) */
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+/* const funcaoTreino = () =>{
+
+let numeroUsuario = +prompt("informe um número")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${numeroUsuario} x ${i} = ${numeroUsuario * i}`)
+}
+}
+
+funcaoTreino() */
